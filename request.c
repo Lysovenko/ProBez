02e2db81095de5699a1f487b37f13be183636426
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2015-04-28 23:24:07 +0300

    REQ_MODEL is active

--- request.c
+++ request.c
@@ -51,19 +51,21 @@ unset_request (int n)
 void
 allocate_requests (void)
 {
-  set_request (REQ_KUPA_3D, calloc (1, sizeof (Elements3D)));
+  Viewpoint *vp= malloc (sizeof (Viewpoint));
+  tensor *tens = malloc (sizeof (tensor));
+  Model *mod = calloc (1, sizeof (Model));
+  Elements3D *set = calloc (1, sizeof (Elements3D));
+  set_request (REQ_KUPA_3D, set);
   set_request (REQ_KUPAS_3D, calloc (1, sizeof (SetsContainer)));
-  {
-    Viewpoint *vp;
-    tensor *tens;
-
-    vp = malloc (sizeof (Viewpoint));
-    vp->vp.x = vp->vp.y = 0.;
-    vp->vp.z = 10.;
-    vp->Z = 8.;
-    set_request (REQ_PT_OF_V, vp);
-    tens = malloc (sizeof (tensor));
-    *tens = InitRotation (0., 0.);
-    set_request (REQ_TENS, tens);
-  }
+  vp->vp.x = vp->vp.y = 0.;
+  vp->vp.z = 10.;
+  vp->Z = 8.;
+  set_request (REQ_PT_OF_V, vp);
+  *tens = InitRotation (0., 0.);
+  set_request (REQ_TENS, tens);
+  mod->set = set;
+  mod->rot = tens;
+  mod->vp = vp;
+  mod->ncorners = 6;
+  set_request (REQ_MODEL, mod);
 }
